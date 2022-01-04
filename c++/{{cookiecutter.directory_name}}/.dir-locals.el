31bd1a3c35b2115834b77437b025b3a236fe9a0a
Ren Odion <smakey18@gmail.com>
2022-01-04 08:09:03 +0000

    Make dirlocal activate in any mode

--- c++/{{cookiecutter.directory_name}}/.dir-locals.el
+++ c++/{{cookiecutter.directory_name}}/.dir-locals.el
@@ -1,4 +1,4 @@
-((c++-mode . ((projectile-project-compile-cmd . {{cookiecutter.projectile_compile}})
-              (projectile-project-configure-cmd . {{cookiecutter.projectile_configure}})
-              (projectile-project-project-test-cmd . {{cookiecutter.projectile_test}})
-              (projectile-project-project-run-cmd . {{cookiecutter.projectile_run}}))))
+((nil . ((projectile-project-compilation-cmd . {{cookiecutter.projectile_compile}})
+         (projectile-project-configure-cmd . {{cookiecutter.projectile_configure}})
+         (projectile-project-test-cmd . {{cookiecutter.projectile_test}})
+         (projectile-project-run-cmd . {{cookiecutter.projectile_run}}))))
