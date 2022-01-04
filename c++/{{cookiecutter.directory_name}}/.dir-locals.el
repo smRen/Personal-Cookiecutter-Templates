65fa5766329995046aa9f1f8f5855d622544153f
Ren Odion <smakey18@gmail.com>
2022-01-04 07:25:37 +0000

    Change directory name for c++

---
+++ c++/{{cookiecutter.directory_name}}/.dir-locals.el
@@ -0,0 +1,4 @@
+((c++-mode . ((projectile-project-compile-cmd . {{cookiecutter.projectile_compile}})
+              (projectile-project-configure-cmd . {{cookiecutter.projectile_configure}})
+              (projectile-project-project-test-cmd . {{cookiecutter.projectile_test}})
+              (projectile-project-project-run-cmd . {{cookiecutter.projectile_run}}))))
--- c++/{{cookiecutter.name}}/.dir-locals.el
+++
@@ -1,4 +0,0 @@
-((c++-mode . ((projectile-project-compile-cmd . {{cookiecutter.projectile-compile}})
-              (projectile-project-configure-cmd . {{cookiecutter.projectile-configure}})
-              (projectile-project-project-test-cmd . {{cookiecutter.projectile-test}})
-              (projectile-project-project-run-cmd . {{cookiecutter.projectile-run}}))))
