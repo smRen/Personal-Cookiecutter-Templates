fc86bdd638445f221e7d300717c17d26dc59dc50
Ren Odion <smakey18@gmail.com>
2022-01-04 06:48:38 +0000

    C++ cookiecutter

---
+++ c++/{{Cookiecutter.directory_name}}/.dir-locals.el
@@ -0,0 +1,4 @@
+((c++-mode . ((projectile-project-compile-cmd . {{cookiecutter.projectile-compile}})
+              (projectile-project-configure-cmd . {{cookiecutter.projectile-configure}})
+              (projectile-project-project-test-cmd . {{cookiecutter.projectile-test}})
+              (projectile-project-project-run-cmd . {{cookiecutter.projectile-run}}))))
