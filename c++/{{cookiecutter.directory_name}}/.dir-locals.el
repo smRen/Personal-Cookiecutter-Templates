5e27a59ff7621dc5ce650824d0a46af75f400718
Ren Odion <smakey18@gmail.com>
2022-01-05 02:45:58 +0000

    Remove unnecessary projectile commands

--- c++/{{cookiecutter.directory_name}}/.dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . ((projectile-project-compilation-cmd . {{cookiecutter.projectile_compile}})
-         (projectile-project-configure-cmd . {{cookiecutter.projectile_configure}})
-         (projectile-project-test-cmd . {{cookiecutter.projectile_test}})
-         (projectile-project-run-cmd . {{cookiecutter.projectile_run}}))))
